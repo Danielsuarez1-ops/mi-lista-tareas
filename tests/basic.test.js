3435cc5aaacef14aa4c4eae08e9f1a29c0557fd4
Danielsuarez1-ops <edison.dsuarez@gmail.com>
2025-09-05 14:24:06 -0500

    Update basic.test.js

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,11 +1,3 @@
 // tests/basic.test.js
 describe('Basic project checks', () => {
-  test('arithmetic works', () => {
-    expect(1 + 1).toBe(2);
-  });
-
-  test('index.html exists', () => {
-    const fs = require;
-    expect(fs.existsSync('index.html')).toBe(true);
-  });
-});
+ 
